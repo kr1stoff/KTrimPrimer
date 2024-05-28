9caf475cac3d19f04f27ea1519be17ee71786142
Kristoff Meng <706060951@qq.com>
2024-05-28 16:47:08 +0800

    240528 行分隔符

--- src/main/java/org/kmlab/Main.java
+++ src/main/java/org/kmlab/Main.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
+        // 获取命令行参数
         Map<String, Object> optionMap = OptionConstructor.getOptionMap(args);
     }
 }
